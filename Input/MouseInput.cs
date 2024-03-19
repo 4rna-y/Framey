5b5a5f5690e5bd1f678057af03842b97d56ccda0
4rnay <ikuyama_kyo@outlook.jp>
2024-03-19 14:26:23 +0900

    add mouse impl

---
+++ Input/MouseInput.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Framey.Input
+{
+    public class MouseInput
+    {
+        public Point GetMousePosition(IInputElement relativeTo = null)
+        {
+            Native.POINT p = new Native.POINT();
+            Native.GetCursorPos(ref p);
+            
+            if (relativeTo is null) return new Point(p.X, p.Y);
+            else return Mouse.GetPosition(relativeTo); 
+        }
+
+        public bool GetIsMouseButtonDown(MouseButtons btn)
+        {
+            if (btn == MouseButtons.Left) return Mouse.LeftButton == MouseButtonState.Pressed;
+            else if (btn == MouseButtons.Right) return Mouse.RightButton == MouseButtonState.Released;
+            else if (btn == MouseButtons.Middle) return Mouse.MiddleButton == MouseButtonState.Pressed;
+            else if (btn == MouseButtons.X1) return Mouse.XButton1 == MouseButtonState.Pressed;
+            else if (btn == MouseButtons.X2) return Mouse.XButton2 == MouseButtonState.Pressed;
+            return false;
+        }
+    }
+    public enum MouseButtons
+    {
+        Left,
+        Middle,
+        Right,
+        X1,
+        X2,
+    }
+}
---
+++ Input/Native.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framey.Input
+{
+    internal class Native
+    {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref POINT pt);
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct POINT
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+    }
+}
